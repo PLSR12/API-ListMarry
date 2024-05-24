d87acafda530433d179a109e1411df0a3be49f55
Pedro Lucas Dos Santos <90657983+PLSR12@users.noreply.github.com>
2024-05-24 16:47:23 -0300

    inittial config project

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import routes from './routes'
+import { resolve } from 'path'
+import errorHandler from './middlewares/errorHandler'
+
+dotenv.config()
+
+const app = express()
+
+routes(app)
+app.use(errorHandler)
+
+app.listen(process.env.APP_PORT || 3100, () => {
+  console.log(`${process.env.APP_NAME} running on port ${process.env.APP_PORT}`)
+})
+
+app.get('/', () => {
+  console.log('to no ar')
+})
+
+export default app
